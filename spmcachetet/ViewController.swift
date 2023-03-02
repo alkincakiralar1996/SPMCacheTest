c02bc3645b3cf265a7389a5ba918a199ca378f65
Alkın Çakıralar <a.cakiralar@gmail.com>
2023-03-02 13:57:43 +0300

    test

---
+++ spmcachetet/ViewController.swift
@@ -0,0 +1,18 @@
+// Created by Alkın Çakıralar for spmcachetet in 2.03.2023
+// Using Swift 5.0
+// You can contact me with a.cakiralar@gmail.com
+// Follow me on https://medium.com/@acakiralar
+// Send friend request on https://www.linkedin.com/in/alkincakiralar/ 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
