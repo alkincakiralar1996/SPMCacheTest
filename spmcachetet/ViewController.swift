eff9d1a24020d839a0f92fa02a64e28ee944cc51
Alkın Çakıralar <a.cakiralar@gmail.com>
2023-03-02 15:41:44 +0300

    asdada

--- spmcachetet/ViewController.swift
+++ spmcachetet/ViewController.swift
@@ -5,11 +5,14 @@
 // Send friend request on https://www.linkedin.com/in/alkincakiralar/ 
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        AF.request("asdadsa")
         // Do any additional setup after loading the view.
     }
 
